a23b2e9080cbfeff3e0c32c13424426d0eedb21d
leYohann <lbarco414@gmail.com>
2022-12-09 10:08:06 +0100

    refacto x<=3

--- nombresRomains.test/NombresRomains.cs
+++ nombresRomains.test/NombresRomains.cs
@@ -2,37 +2,18 @@ namespace nombresRomains.test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void test1()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void testInferieurA3(int x)
         {
-            //ETANT DONNE le nombre 1
-            const int nombrearabe = 1;
-            // QUAND on le convertit en nombre romain
-            var resultat = ConvertisseurNombreRomain.Convertir(nombrearabe);
-            // ALORS on obtient 'I'
-            Assert.Equal("I", resultat);
-        }
-
-        [Fact]
-        public void test2()
-        {
-            //ETANT DONNE le nombre 2
-            const int nombrearabe = 2;
-            //QUAND on le convertit en nomnbre romain
-            var resultat = ConvertisseurNombreRomain.Convertir(nombrearabe);
-            //ALORS on obtient 'II'
-            Assert.Equal("II", resultat);
-        }
-
-        [Fact]
-        public void test3()
-        {
-            //ETANT DONNE le nombre 3
-            const int nombrearabe = 3;
-            //QUAND on le convertit en nomnbre romain
-            var resultat = ConvertisseurNombreRomain.Convertir(nombrearabe);
-            //ALORS on obtient 'III'
-            Assert.Equal("III", resultat);
+            //ETANT DONNE un nombre x<3
+            //Quand on le convertit en nombre romain
+            var resultat = ConvertisseurNombreRomain.Convertir(x);
+            //On obtient x fois "I"
+            var attendu = new string('I', x);
+            Assert.Equal(attendu, resultat);
         }
     }
 }
